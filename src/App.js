f0e347bbbec8838a9b316500f9427cb16a3da0e6
tmclean85 <trevor.a.mclean@gmail.com>
2018-02-01 23:54:50 -0800

    Make fetch request, map API data, foiled by CORS

--- src/App.js
+++ src/App.js
@@ -1,20 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:8000/notes/5a73f33156b6405b6fe76d19")
+    .then(res => res.json())
+    .then(
+      (result) => {
+        this.setState({
+          isLoaded: true,
+          items: result.items
+        });
+      },
+      (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
+      }
+    )
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading....</div>;
+    } else {
+      return (
+        <ul>
+          {items.map(item => (
+            <li key={item._id}>
+              {item.speaker}
+            </li>  
+          ))}
+        </ul>  
+      );
+    }
   }
 }
 
